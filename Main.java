73a1c85ace789a4f02b2e50dbf5f2a6bb3736a51
krish <krishnabadde115@gmail.com>
2025-02-08 00:19:31 +0530

    Added some class

---
+++ Main.java
@@ -0,0 +1,9 @@
+public call Main(){
+    public static void main(String args[]){
+        System.out.println("Hi in JAVA Program");
+
+        System.out.println("Whats app");
+
+        System.out.println("Ending it");
+    }
+}
